c116fbda207a7bb16f9c9833519ec7f13816b548
Ho Duc Dong <donghdd00342@fpt.edu.vn>
2020-10-07 00:15:24 +0700

    konami code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -338,3 +338,32 @@ function say(m) {
   msg.lang = document.getElementById('asLangSay').value;
   speechSynthesis.speak(msg);
 }
+
+// BEGIN Konami Code
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, action and reset
+	if (pattern.length === current) {
+    current = 0;
+    try {
+      document.getElementById('konamiHide').style.display = 'initial';
+    } catch (error) {}
+	}
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
+// END Konami Code
